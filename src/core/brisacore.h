2f8cb86d2103d3b9bf6b3b553237e56e87eedcdd
jeysibel <jeysibel@0dea1104-2826-0410-b20c-9c2201499e24>
2010-04-08 02:53:47 +0000

    - Review of Doxygen Comments in header files - Put a New Header Format with LGPL license disclaimer, BRisa-Qt name, and some metadata.
    git-svn-id: https://vcs.maemo.org/svn/brisa/qt/trunk/qt-brisa@1765 0dea1104-2826-0410-b20c-9c2201499e24

--- src/core/brisacore.h
+++ src/core/brisacore.h
@@ -1,23 +1,31 @@
-/* brisa-c++
+/*
+ * Universidade Federal de Campina Grande
+ * Centro de Engenharia Elétrica e Informática
+ * Laboratório de Sistemas Embarcados e Computação Pervasiva
+ * BRisa / BRisa-Qt
+ * Filename: brisaconfig.cpp
+ * Created:
+ * Description:
+ * Authors: Andre Dieb Martins <andre.dieb@gmail.com> @since 2009
  *
- * Copyright (C) 2009 Andre Dieb Martins <andre.dieb@gmail.com>
  *
- * This file is part of brisa-c++.
+ * Copyright (C) <2009> <Embbeded Systems and Pervasive Computing Laboratory>
  *
- * brisa-c++ is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * BRisa-Qt is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
  *
- * brisa-c++ is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with brisa-c++.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  */
+
 
 #ifndef _BRISACORE_H
 #define _BRISACORE_H
