ef529041777e8c38d4adf04e959e9a0b8fb38590
vinipsmaker <vinipsmaker@0dea1104-2826-0410-b20c-9c2201499e24>
2010-08-10 01:47:12 +0000

    Changing license of the webserver from GPL to LGPL
    git-svn-id: https://vcs.maemo.org/svn/brisa/qt/trunk/qt-brisa@1842 0dea1104-2826-0410-b20c-9c2201499e24

--- src/core/webserver_new/httpserver.h
+++ src/core/webserver_new/httpserver.h
@@ -1,19 +1,30 @@
 /*
-  Copyright (C) 2010 Vinícius dos Santos Oliveira
-
-  This program is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License
-  version 2 as published by the Free Software Foundation.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-*/
+ * Universidade Federal de Campina Grande
+ * Centro de Engenharia Elétrica e Informática
+ * Laboratório de Sistemas Embarcados e Computação Pervasiva
+ * BRisa Project / BRisa-Qt - http://brisa.garage.maemo.org
+ * Filename: httpserver.h
+ * Created:
+ * Description: Implementation of BrisaConfigurationManager class.
+ * Authors: Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com> @since 2010
+ *
+ *
+ * Copyright (C) <2009> <Embbeded Systems and Pervasive Computing Laboratory>
+ *
+ * BRisa-Qt is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 #ifndef HTTPSERVER_H
 #define HTTPSERVER_H
--- src/core/webserver_new/tcpserver.h
+++ src/core/webserver_new/tcpserver.h
@@ -1,3 +1,31 @@
+/*
+ * Universidade Federal de Campina Grande
+ * Centro de Engenharia Elétrica e Informática
+ * Laboratório de Sistemas Embarcados e Computação Pervasiva
+ * BRisa Project / BRisa-Qt - http://brisa.garage.maemo.org
+ * Filename: tcpserver.h
+ * Created:
+ * Description: Implementation of BrisaConfigurationManager class.
+ * Authors: Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com> @since 2010
+ *
+ *
+ * Copyright (C) <2009> <Embbeded Systems and Pervasive Computing Laboratory>
+ *
+ * BRisa-Qt is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef TCPSERVER_H
 #define TCPSERVER_H
 
